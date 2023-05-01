da44625762d6d44d972a33088cad9ca325a7e0f7
Pierre-Richard <pierre.richard.prb@gmail.com>
2023-05-01 23:08:11 +0200

    Final version 1.0

--- src/utils/var.js
+++ src/utils/var.js
@@ -1,270 +1,274 @@
 export const CUISINE = [{
         name: "African",
-        image: "src/assets/images/cuisines/african.jpg"
+        image: "images/cuisines/african.jpg"
     },
     {
         name: "American",
-        image: "src/assets/images/cuisines/american.jpg"
+        image: "images/cuisines/american.jpg"
     },
     {
         name: "Cajun",
-        image: "src/assets/images/cuisines/cajun.jpg"
+        image: "images/cuisines/cajun.jpg"
     },
     {
         name: "Caribbean",
-        image: "src/assets/images/cuisines/caribbean.jpg"
+        image: "images/cuisines/caribbean.jpg"
     },
     {
         name: "Chinese",
-        image: "src/assets/images/cuisines/chinese.jpg"
+        image: "images/cuisines/chinese.jpg"
     },
     {
         name: "French",
-        image: "src/assets/images/cuisines/french.jpg"
+        image: "images/cuisines/french.jpg"
     },
     {
         name: "German",
-        image: "src/assets/images/cuisines/german.jpg"
+        image: "images/cuisines/german.jpg"
     },
     {
         name: "Greek",
-        image: "src/assets/images/cuisines/greek.jpg"
+        image: "images/cuisines/greek.jpg"
     },
     {
         name: "Indian",
-        image: "src/assets/images/cuisines/indian.jpg"
+        image: "images/cuisines/indian.jpg"
     },
     {
         name: "Irish",
-        image: "src/assets/images/cuisines/irish.jpg"
+        image: "images/cuisines/irish.jpg"
     },
     {
         name: "Italian",
-        image: "src/assets/images/cuisines/italian.jpg"
+        image: "images/cuisines/italian.jpg"
     },
     {
         name: "Japanese",
-        image: "src/assets/images/cuisines/japanese.jpg"
+        image: "images/cuisines/japanese.jpg"
     },
     {
         name: "Korean",
-        image: "src/assets/images/cuisines/korean.jpg"
+        image: "images/cuisines/korean.jpg"
     },
     {
         name: "Mediterranean",
-        image: "src/assets/images/cuisines/mediterranean.jpg"
+        image: "images/cuisines/mediterranean.jpg"
     },
     {
         name: "Mexican",
-        image: "src/assets/images/cuisines/mexican.jpg"
+        image: "images/cuisines/mexican.jpg"
     },
     {
         name: "Spanish",
-        image: "src/assets/images/cuisines/spanish.jpg"
+        image: "images/cuisines/spanish.jpg"
     },
     {
         name: "Thai",
-        image: "src/assets/images/cuisines/thai.jpg"
+        image: "images/cuisines/thai.jpg"
     },
     {
         name: "Vietnamese",
-        image: "src/assets/images/cuisines/vietnamese.jpg"
+        image: "images/cuisines/vietnamese.jpg"
     },
 ]
 
 export const DIET = [{
         name: "Gluten Free",
-        image: "src/assets/images/diets/no_gluten.svg"
+        image: "images/diets/no_gluten.svg"
     },
     {
         name: "Ketogenic",
-        image: "src/assets/images/diets/avocado.svg"
+        image: "images/diets/avocado.svg"
     },
     {
         name: "Vegetarian",
-        image: "src/assets/images/diets/dairy.svg"
+        image: "images/diets/dairy.svg"
     },
     {
         name: "Lacto-Vegetarian",
-        image: "src/assets/images/diets/milk.svg"
+        image: "images/diets/milk.svg"
     },
     {
         name: "Ovo-Vegetarian",
-        image: "src/assets/images/diets/eggs.svg"
+        image: "images/diets/eggs.svg"
     },
     {
         name: "Vegan",
-        image: "src/assets/images/diets/leaf.svg"
+        image: "images/diets/leaf.svg"
     },
     {
         name: "Pescetarian",
-        image: "src/assets/images/diets/fish.svg"
+        image: "images/diets/fish.svg"
     },
     {
         name: "Paleo",
-        image: "src/assets/images/diets/chicken.svg"
+        image: "images/diets/chicken.svg"
     },
     {
         name: "Primal",
-        image: "src/assets/images/diets/meat.svg"
+        image: "images/diets/meat.svg"
     },
     {
         name: "Low FODMAP",
-        image: "src/assets/images/diets/fruits.svg"
+        image: "images/diets/fruits.svg"
     }
 ]
 
 export const INTOLERANCES = [{
         name: "Dairy",
-        image: "src/assets/images/intolerances/dairy.svg"
+        image: "images/intolerances/dairy.svg"
     },
     {
         name: "Egg",
-        image: "src/assets/images/intolerances/eggs.svg"
+        image: "images/intolerances/eggs.svg"
 
     },
     {
         name: "Gluten",
-        image: "src/assets/images/intolerances/gluten.svg"
+        image: "images/intolerances/gluten.svg"
 
     },
     {
         name: "Peanut",
-        image: "src/assets/images/intolerances/peanut.svg"
+        image: "images/intolerances/peanut.svg"
 
     },
     {
         name: "Seafood",
-        image: "src/assets/images/intolerances/seafood.svg"
+        image: "images/intolerances/seafood.svg"
 
     },
     {
         name: "Sesame",
-        image: "src/assets/images/intolerances/sesame.svg"
+        image: "images/intolerances/sesame.svg"
 
     },
     {
         name: "Shellfish",
-        image: "src/assets/images/intolerances/shellfish.svg"
+        image: "images/intolerances/shellfish.svg"
 
     },
     {
         name: "Soy",
-        image: "src/assets/images/intolerances/soy.svg"
+        image: "images/intolerances/soy.svg"
 
     },
     {
         name: "Sulfite",
-        image: "src/assets/images/intolerances/sulfite.svg"
+        image: "images/intolerances/sulfite.svg"
 
     },
     {
         name: "Wheat",
-        image: "src/assets/images/intolerances/wheat.svg"
+        image: "images/intolerances/wheat.svg"
 
     },
 ]
 
 export const TYPE = [{
         name: "main course",
-        image: "src/assets/images/types/main_dish.svg"
+        image: "images/types/main_dish.svg"
     },
     {
         name: "side dish",
-        image: "src/assets/images/types/side_dish.svg"
+        image: "images/types/side_dish.svg"
     },
     {
         name: "dessert",
-        image: "src/assets/images/types/dessert.svg"
+        image: "images/types/dessert.svg"
     },
     {
         name: "appetizer",
-        image: "src/assets/images/types/appetizer.svg"
+        image: "images/types/appetizer.svg"
     },
     {
         name: "salad",
-        image: "src/assets/images/types/salad.svg"
+        image: "images/types/salad.svg"
     },
     {
         name: "bread",
-        image: "src/assets/images/types/bread.svg"
+        image: "images/types/bread.svg"
     },
     {
         name: "breakfast",
-        image: "src/assets/images/types/breakfast.svg"
+        image: "images/types/breakfast.svg"
     },
     {
         name: "soup",
-        image: "src/assets/images/types/soup.svg"
+        image: "images/types/soup.svg"
     },
     {
         name: "beverage",
-        image: "src/assets/images/types/beverage.svg"
+        image: "images/types/beverage.svg"
     },
     {
         name: "sauce",
-        image: "src/assets/images/types/sauce.svg"
+        image: "images/types/sauce.svg"
     },
     {
         name: "fingerfood",
-        image: "src/assets/images/types/fingerfood.svg"
+        image: "images/types/fingerfood.svg"
     },
     {
         name: "snack",
-        image: "src/assets/images/types/snack.svg"
+        image: "images/types/snack.svg"
     },
     {
         name: "drink",
-        image: "src/assets/images/types/drink.svg"
+        image: "images/types/drink.svg"
     },
 ]
 
 export const UNWANTEDINGREDIENTS = [{
         name: "BEEF",
-        image: "src/assets/images/unwanted_ingredients/beef.svg"
+        image: "images/unwanted_ingredients/beef.svg"
     },
     {
         name: "PORK",
-        image: "src/assets/images/unwanted_ingredients/pork.svg"
+        image: "images/unwanted_ingredients/pork.svg"
     },
     {
         name: "FISH",
-        image: "src/assets/images/unwanted_ingredients/fish.svg"
+        image: "images/unwanted_ingredients/fish.svg"
     },
     {
         name: "SUGAR",
-        image: "src/assets/images/unwanted_ingredients/sugar.svg"
+        image: "images/unwanted_ingredients/sugar.svg"
     },
     {
         name: "TOMATOES",
-        image: "src/assets/images/unwanted_ingredients/tomatoes.svg"
+        image: "images/unwanted_ingredients/tomatoes.svg"
     },
     {
         name: "SEAFOOD",
-        image: "src/assets/images/unwanted_ingredients/crab.svg"
+        image: "images/unwanted_ingredients/crab.svg"
     },
     {
         name: "COCONUT",
-        image: "src/assets/images/unwanted_ingredients/coconut.svg"
+        image: "images/unwanted_ingredients/coconut.svg"
     },
     {
         name: "EGGPLANT",
-        image: "src/assets/images/unwanted_ingredients/eggplant.svg"
+        image: "images/unwanted_ingredients/eggplant.svg"
     },
     {
         name: "ONIONS",
-        image: "src/assets/images/unwanted_ingredients/onion.svg"
+        image: "images/unwanted_ingredients/onion.svg"
     },
     {
         name: "OLIVE",
-        image: "src/assets/images/unwanted_ingredients/olives.svg"
+        image: "images/unwanted_ingredients/olives.svg"
     },
     {
         name: "ALCOHOL",
-        image: "src/assets/images/unwanted_ingredients/drink.svg"
+        image: "images/unwanted_ingredients/drink.svg"
     },
     {
         name: "AVOCADO",
-        image: "src/assets/images/unwanted_ingredients/avocado.svg"
+        image: "images/unwanted_ingredients/avocado.svg"
     },
 ]
+
+
+
+
